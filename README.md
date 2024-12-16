04531826d0c91b66b84f9227a4eb2785f035121b
Delta-11 <exdelta11@gmail.com>
2024-12-16 10:27:02 +0530

    Nexus repo is done

---
+++ README.md
@@ -0,0 +1,1 @@
+THis is readme for calc file.
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,7 +1,7 @@
 # tests/test_main.py
 import pytest
 from fastapi.testclient import TestClient
-from ..main import app
+from src.calc_backend.main import app
 
 client = TestClient(app)
 
